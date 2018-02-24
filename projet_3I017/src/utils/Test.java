6fe41a4abcfc32e742cb733ff9c17f1aa91d4108
fodza <fodza@DESKTOP-6AIJ29S>
2018-02-25 00:02:10 +0100

    Test (fait)

--- projet_3I017/src/utils/Test.java
+++ projet_3I017/src/utils/Test.java
@@ -11,28 +11,33 @@ public class Test
 	{
 		System.out.println(new ErrorJSON());
 		// Les infos de l'utilisateur
-		String login = "JS";
+		String login = "tata";
 		String pwd = "mot de passe de toto";
 		String prenom = "Jason";
 		String nom = "Statham";
 		String mail = "toto@chips.fr";
-		String loginFriend = "toto";
+		String loginFriend = "tata";
 		int root = 1;
-		int idFriend = base_de_donnees.UserTools.getIdUser(loginFriend); 
-		int id;
+		int idUser = base_de_donnees.UserTools.getIdUser(loginFriend); 
+		int id1;
+		int id2;
 		
 		System.out.println(service.UserServices.createUser(login, pwd, prenom, nom, mail).toString());
-		id = base_de_donnees.UserTools.getIdUser(login);
+		id1 = base_de_donnees.UserTools.getIdUser(login);
+		id2= base_de_donnees.UserTools.getIdUser("titi");
+		//System.out.println("Insérer connexion : " + base_de_donnees.UserTools.insererConnexion(login, pwd, root) + "\n");
 		
-		System.out.println("Insérer connexion : " + base_de_donnees.UserTools.insererConnexion(login, pwd, root) + "\n");
-		String key = base_de_donnees.UserTools.getKey(id);
-		System.out.println("isConnexion : " + base_de_donnees.UserTools.isConnection(key) + "\n");
+		//String key = base_de_donnees.UserTools.getKey(id2);
+		//System.out.println("isConnexion : " + base_de_donnees.UserTools.isConnection(key) + "\n");
+
+		System.out.println("search friend friend : " + base_de_donnees.UserTools.searchUserByLogin("t"));
 		
-		String id_msg= "5a8d9299e4b0893a2034300b";
 		
-		base_de_donnees.MessageTools.addMessage(""+id, "à oilp");
+		//String id_msg= "5a8d9299e4b0893a2034300b";
+		
+		//base_de_donnees.MessageTools.addMessage(""+id, "à oilp");
 		//base_de_donnees.MessageTools.removeMessage(id_msg);
-		System.out.println("Liste des messages : \n" + base_de_donnees.MessageTools.listMessage(""+id, false, 2).toString() + "\n");
+		//System.out.println("Liste des messages : \n" + base_de_donnees.MessageTools.listMessage(""+id, false, 2).toString() + "\n");
 	}
 
 }
